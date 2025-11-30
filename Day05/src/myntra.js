47ff063dd6d29783c7c26cbce7b2a327c8f99645
suvajit samanta <166320455+suvajit2005@users.noreply.github.com>
2025-11-30 19:44:04 +0530

    day05 uploaded

---
+++ Day05/src/myntra.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+import Card from "./component/Card";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import arr from "./utils/dummy"
+import {greet, meet} from "./utils/dummy"
+
+
+function App(){
+  return(<>
+
+    {/* Header */}
+    <Header/>
+
+
+
+    {/* main */}
+
+    <div className="middle" style={{display:"flex", gap:"10px" , flexWrap:"wrap"}}>
+        {
+          arr.map((value,index)=> <Card key={index} cloth={value.cloth} offer={value.Offer}/>)   
+        }
+       
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+
+
+
+const Root = ReactDOM.createRoot(document.getElementById('root'));
+Root.render(<App/>);
