f6e42840e8b479e123be5052e1ae2c624be9e3de
suvajit samanta <166320455+suvajit2005@users.noreply.github.com>
2025-12-10 23:52:33 +0530

    Day 14 uploaded

---
+++ Day14/Stores.js
@@ -0,0 +1,22 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import slice1Reducer from "./Slicer1"
+
+// const stores = configureStore({
+//     reducer:{
+//       slice1: slice1Reducer,
+//     }
+// })
+
+// export default stores;
+
+// slice name: Reducer
+import { configureStore } from "@reduxjs/toolkit";
+
+import slice1Reducer from "./Slicer1"
+const stores=configureStore({
+  reducer:{
+    slice1:slice1Reducer,
+  }
+
+})
+export default stores;
